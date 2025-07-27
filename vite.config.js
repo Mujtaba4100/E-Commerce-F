55bdb5b2c5fe5e736691d38e3a12c70a70ea1fcb
Mujtaba4100 <saad444hassan@gmail.com>
2025-07-27 23:13:53 +0500

    chk7

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,10 @@ export default defineConfig({
         cart: resolve(__dirname, 'public/cart.html'),
         register: resolve(__dirname, 'public/register.html'),
         order: resolve(__dirname, 'public/order.html'),
-        admin: resolve(__dirname, 'public/admin.html')
+        admin: resolve(__dirname, 'public/admin.html'),
+        checkout: resolve(__dirname, 'public/checkout.html'),
+        product: resolve(__dirname, 'public/product.html'),
+        products: resolve(__dirname, 'public/products.html')
       }
     }
   }
